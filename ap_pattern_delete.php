f18539b578e6bec5fd976a1179b5160d6952ecef
Nonnesha Chomsee <nonnesha.c@lotte.local>
2020-09-22 18:03:03 +0700

    Nook

---
+++ ap_pattern_delete.php
@@ -0,0 +1,85 @@
+<?php
+    include("connect.php");
+    require("connect.php");
+
+    $str_id = "";
+
+    $str_id= $_GET['hd_ap_pattern_id'];
+
+    $sql = "SELECT * FROM `ap_pattern_tb` WHERE ap_pattern_id= '". $str_id ."' ";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AP Pattern Edit</title>
+
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/correspondent_form.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/global.css" />
+</head>
+<body>
+    <form action="r_ap_pattern_edit.php" method="post">
+        <div class="contrainer-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>AP Pattern Edit</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3">
+                    <h3>AP Pattern ID</h3>
+                </div>
+                <div class="col-md-6">
+                    <?=$row['ap_pattern_id']?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3">
+                    <h3>AP Pattern Code</h3>
+                </div>
+                <div class="col-md-6">
+                    <?=$row['ap_pattern_code']?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3">
+                    <h3>AP Pattern Name1</h3>
+                </div>
+                <div class="col-md-6">
+                    <input id="txt_ap_pattern_name1" type="text" name="txt_ap_pattern_name1" value="<?=$row['ap_pattern_name1']?>">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3">
+                    <h3>AP Pattern Name2</h3>
+                </div>
+                <div class="col-md-6">
+                    <input id="txt_ap_pattern_name2" type="text" name="txt_ap_pattern_name2" value="<?=$row['ap_pattern_name2']?>">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3">
+                    <h3>AP Pattern Status</h3>
+                </div>
+                <div class="col-md-6">
+                    <input id="txt_ap_pattern_status" type="text" name="txt_ap_pattern_status" value="<?=$row['ap_pattern_status']?>">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 input">
+                <input id="txt_ap_pattern_code" type="hidden" name="txt_ap_pattern_code" value="<?=$row['ap_pattern_code']?>">
+                    <input id="txt_ap_pattern_id" type="hidden" name="txt_ap_pattern_id" value="<?=$row['ap_pattern_id']?>">
+                    <input class="btn btn-primary" id="sbt" type="Submit" name="btnsave" value="Save">
+                    <input class="btn btn-primary" id="sbt" type="Submit" name="btnclr" value="Clear">
+                </div>
+            </div> 
+        </div>
+    </form>
+</body>
+</html>
---
+++ r_ap_pattern_delete.php
@@ -0,0 +1,26 @@
+<?php
+    include("connect.php");
+    require("connect.php");
+
+    $str_ap_pattern_id = $_POST['txt_ap_pattern_id'];
+    $str_ap_pattern_code = $_POST['txt_ap_pattern_code'];
+    $str_ap_pattern_name1 = $_POST['txt_ap_pattern_name1'];
+    $str_ap_pattern_name2 = $_POST['txt_ap_pattern_name2'];
+    $str_ap_pattern_status = $_POST['txt_ap_pattern_status'];
+
+    $str_sql = "UPDATE `ap_pattern_tb` SET ";
+	$str_sql .= " `ap_pattern_code`= '" . $str_ap_pattern_code . "',";
+    $str_sql .= " `ap_pattern_name1`= '" . $str_ap_pattern_name1 . "',";
+    $str_sql .= " `ap_pattern_name2`= '" . $str_ap_pattern_name2 . "',";
+    $str_sql .= " `ap_pattern_status`= '0' ";
+    $str_sql .= " WHERE `ap_pattern_id` = '" . $str_ap_pattern_id . "'";
+
+    if ( mysqli_query($conn,$str_sql) ) {
+		//echo "Save OK !";
+		header("location:ap_pattern_manage.php");
+		exit;
+	} else {
+		echo "Save Error ! : " . $str_sql;
+	}
+    
+?>
